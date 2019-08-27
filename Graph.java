6d97cc9b4a4238be2c373d533a3248e7bbc210ef
svr8 <shikhar.vaish90@gmail.com>
2019-08-27 22:58:55 +0530

    added floydwarshall

--- Graph.java
+++ Graph.java
@@ -111,3 +111,22 @@ class TreeNode {
 
   }
 }
+
+// SHORTEST PATH ALGORITHMS -----------------------------
+
+static long[][] floydWarshall(long[][] graph) {
+  int V = graph.length;
+  long[][] d = new long[V][V];
+
+  for(int i=0;i<V;i++)
+    for(int j=0;j<V;j++)
+      d[i][j] = graph[i][j];
+  
+  for(int k=0;k<V;k++)
+    for(int i=0;i<V;i++)
+      for(int j=0;j<V;j++)
+        if(d[i][k]+d[k][j]<d[i][j])
+          d[i][j] = d[i][k]+d[k][j];
+  
+  return d;
+}
